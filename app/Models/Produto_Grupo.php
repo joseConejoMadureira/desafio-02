caa4eb62796a5de18fc4ad043e253f6d645f6f28
joseConejoMadureira <jose.games15@outlook.com>
2023-09-02 16:08:45 -0300

    Model  E Migrations

---
+++ app/Models/Produto_Grupo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produto_Grupo extends Model
+{
+    use HasFactory;
+}
