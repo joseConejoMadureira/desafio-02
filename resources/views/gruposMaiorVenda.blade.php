1fd02814abbabc951e1e31b012ed21147913ad67
joseConejoMadureira <jose.games15@outlook.com>
2023-09-03 20:48:04 -0300

    view

--- resources/views/gruposMaiorVenda.blade.php
+++ resources/views/gruposMaiorVenda.blade.php
@@ -1,8 +1,21 @@
 <html>
 
 <body>
-    {{ $gruposMaiorVenda  }}
+    <table class="ui celled table">
+        <thead>
+            <tr>
+                <th>Produto Grupo</th>
+
+            </tr>
+        </thead>
+        @foreach ($gruposMaiorVenda as $value)
+            <tbody>
+
+                <td>{{ $value->nm_produto_grupo }}</td>
+                </tr>
+            </tbody>
+        @endforeach
+    </table>
 </body>
 
 </html>
-
