2c672d0449c620eaf3328bf8069d136b29576b5c
joseConejoMadureira <jose.games15@outlook.com>
2023-09-03 14:12:55 -0300

    timestamps

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
 }
--- app/Models/PedidoItem.php
+++ app/Models/PedidoItem.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class PedidoItem extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
 }
