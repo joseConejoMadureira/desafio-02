401f3dfb8773b1daf225087a3cf3410f8f451f1c
joseConejoMadureira <jose.games15@outlook.com>
2023-09-03 15:09:52 -0300

    5 resultados

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -14,6 +14,8 @@ class PedidoController extends Controller
                                               ->join('produtos', 'pedido_itens.id_produto', '=', 'produtos.id_produto')
                                               ->join('produto_grupos', 'produtos.id_produto_grupo', '=', 'produto_grupos.id_produto_grupo')
                                               ->select('produto_grupos.nm_produto_grupo')
+                                              ->orderBy('pedido_itens.qt_produto','desc')
+                                              ->take(5)
                                              ->get();
 
 
