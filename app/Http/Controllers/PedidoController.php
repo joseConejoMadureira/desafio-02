368d8ef5594b940088981ec40ae75f4b9f2408fd
joseConejoMadureira <jose.games15@outlook.com>
2023-09-03 21:25:24 -0300

    maiorVenda

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -24,6 +24,21 @@ class PedidoController extends Controller
         return view ('gruposMaiorVenda')->with('gruposMaiorVenda',$gruposMaiorVenda);
     }
 
+    function maiorVenda() {
 
+
+        $gruposMaiorVenda = PedidoItem::join('pedidos', 'pedido_itens.id_pedido', '=', 'pedidos.id_pedido')
+                                              ->join('produtos', 'pedido_itens.id_produto', '=', 'produtos.id_produto')
+                                              ->join('produto_grupos', 'produtos.id_produto_grupo', '=', 'produto_grupos.id_produto_grupo')
+                                              ->whereYear('pedidos.dt_pedido', '=', 2023)
+                                              ->whereMonth('pedidos.dt_pedido', '=', 8)
+                                              ->select('produto_grupos.nm_produto_grupo')
+                                              ->orderBy('pedido_itens.qt_produto','desc')
+                                              ->take(5)
+                                              ->get()
+                                              ->unique("nm_produto_grupo");
+
+        return view ('maiorVenda')->with('gruposMaiorVenda',$gruposMaiorVenda);
+    }
 
 }
