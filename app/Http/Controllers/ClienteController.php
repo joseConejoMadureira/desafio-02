b45ee63cccd7a68a21f0746a69c0803171049830
joseConejoMadureira <jose.games15@outlook.com>
2023-09-04 12:28:29 -0300

    "IDOSO"

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\For_;
 
 class ClienteController extends Controller
 {
     function idoso() {
 
-        Cliente::where('dt_nascimento' ,'<=', '1953-09-3')->update(['nm_cliente'=>'idoso']);
+
+        $idodo =  Carbon::now()->subYears(70)->toDateString();
+
+        Cliente::where('dt_nascimento' ,'<=', $idodo)->update(['nm_cliente'=>'IDOSO']);
 
         return redirect('/');
     }
