e81f2524c7160e2a0190df4214d9d798cbbf52c6
joseConejoMadureira <jose.games15@outlook.com>
2023-09-11 20:10:53 -0300

    ProdutoGrupoSeeder

--- database/seeders/ProdutoGrupoSeeder.php
+++ database/seeders/ProdutoGrupoSeeder.php
@@ -2,16 +2,44 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProdutoGrupo;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ProdutoGrupoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        //
+        ProdutoGrupo::insert([
+            [
+                'nm_produto_grupo' => 'console',
+
+            ],
+            [
+                'nm_produto_grupo' => 'celulares',
+
+            ],
+            [
+                'nm_produto_grupo' => 'moveis',
+
+            ],
+            [
+                'nm_produto_grupo' => 'eletrodomesticos',
+
+            ],
+            [
+                'nm_produto_grupo' => 'roupas',
+
+            ],
+            [
+                'nm_produto_grupo' => 'acessorios',
+
+            ],
+            [
+                'nm_produto_grupo' => 'calcados',
+
+            ]
+        ]);
     }
 }
