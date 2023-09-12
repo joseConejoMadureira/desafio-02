f656b1774152be546f5a47b4829f8358c7212df2
joseConejoMadureira <jose.games15@outlook.com>
2023-09-12 16:45:07 -0300

    Selecione os dias com maior venda no ano de 2023

--- database/seeders/PedidoItemSeeder.php
+++ database/seeders/PedidoItemSeeder.php
@@ -2,16 +2,94 @@
 
 namespace Database\Seeders;
 
+use App\Models\PedidoItem;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PedidoItemSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        //
+
+        //5) Selecione os dias com maior venda no ano de 2023;
+        PedidoItem::insert([
+            [
+                'qt_produto' => 150,
+                'id_pedido' => 1,
+                'id_produto' => 1
+            ],
+            [
+                'qt_produto' => 500,
+                'id_pedido' => 1,
+                'id_produto' => 1
+            ],
+            [
+                'qt_produto' => 400,
+                'id_pedido' => 2,
+                'id_produto' => 2
+            ],
+            [
+                'qt_produto' => 300,
+                'id_pedido' => 3,
+                'id_produto' => 3
+            ],
+            [
+                'qt_produto' => 200,
+                'id_pedido' => 4,
+                'id_produto' => 4
+            ],
+            [
+                'qt_produto' => 100,
+                'id_pedido' => 5,
+                'id_produto' => 5
+            ],
+            [
+                'qt_produto' => 50,
+                'id_pedido' => 6,
+                'id_produto' => 6
+            ]
+
+        ]);
+        /*
+        PedidoItem::insert([
+            [
+                'qt_produto' => 150,
+                'id_pedido' => 1,
+                'id_produto' => 1
+            ],
+            [
+                'qt_produto' => 500,
+                'id_pedido' => 1,
+                'id_produto' => 1
+            ],
+            [
+                'qt_produto' => 400,
+                'id_pedido' => 2,
+                'id_produto' => 2
+            ],
+            [
+                'qt_produto' => 300,
+                'id_pedido' => 3,
+                'id_produto' => 3
+            ],
+            [
+                'qt_produto' => 200,
+                'id_pedido' => 4,
+                'id_produto' => 4
+            ],
+            [
+                'qt_produto' => 100,
+                'id_pedido' => 5,
+                'id_produto' => 5
+            ],
+            [
+                'qt_produto' => 50,
+                'id_pedido' => 6,
+                'id_produto' => 6
+            ]
+
+        ]);
+        */
     }
 }
--- database/seeders/PedidoSeeder.php
+++ database/seeders/PedidoSeeder.php
@@ -2,16 +2,96 @@
 
 namespace Database\Seeders;
 
+use App\Models\Pedido;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PedidoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        //
+       // 5) Selecione os dias com maior venda no ano de 2023;
+        Pedido::insert([
+            [
+                'dt_pedido' => '2023-07-01',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-02',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-03',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-04',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-05',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-06',
+                'id_cliente' => 6,
+
+
+            ]
+
+        ]);
+
+        /*
+        Pedido::insert([
+            [
+                'dt_pedido' => '2023-07-01',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-02',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-03',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-04',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-05',
+                'id_cliente' => 6,
+
+
+            ],
+            [
+                'dt_pedido' => '2023-07-06',
+                'id_cliente' => 6,
+
+
+            ]
+
+        ]);
+        */
     }
 }
