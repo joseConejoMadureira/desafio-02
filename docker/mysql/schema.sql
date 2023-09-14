888c19342cd0c4d273f9d0cc0bf29c2c220d1abe
joseConejoMadureira <jose.games15@outlook.com>
2023-09-14 15:23:22 -0300

    mysql

--- docker/mysql/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE laravel;
-
-USE laravel;
-
-CREATE TABLE `laravel`.`log` (
-  	`id` bigint NOT NULL AUTO_INCREMENT,
-  	`description` varchar(100) DEFAULT NULL,
-  	PRIMARY KEY (`id`)
-	);
